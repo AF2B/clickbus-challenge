86aaffecb9be0fd1e07d366b3e99b3abbc27544f
andre.borba <dev_andre.filipe@outlook.com>
2023-09-07 20:09:33 -0300

    Place Request

---
+++ src/main/java/br/com/challenge/clickbuschallenge/api/PlaceRequest.java
@@ -0,0 +1,11 @@
+package br.com.challenge.clickbuschallenge.api;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record PlaceRequest(
+    @NotBlank String name,
+    @NotBlank String city,
+    @NotBlank String state
+) {
+    
+}
