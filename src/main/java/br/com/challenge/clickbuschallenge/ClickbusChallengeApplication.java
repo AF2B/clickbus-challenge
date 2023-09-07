fd9fbc8f26f83a79a5bc91c7e17b526cade0b262
andre.borba <dev_andre.filipe@outlook.com>
2023-09-07 20:10:08 -0300

    Main Application

---
+++ src/main/java/br/com/challenge/clickbuschallenge/ClickbusChallengeApplication.java
@@ -0,0 +1,13 @@
+package br.com.challenge.clickbuschallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickbusChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClickbusChallengeApplication.class, args);
+	}
+
+}
